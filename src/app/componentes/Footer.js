713213ce6fcf1c2bcabf79ece15c99502e021899
aniteixeira <lisagoncalves.1977@gmail.com>
2024-06-07 10:54:46 -0400

    Ultimas atualizações
    Alterei as importações, adicionei as logos, fiz o css do footer e ajustei algumas partes do header.

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,17 +1,47 @@
-import Image from "next/image";
 import Styles from "./footer.module.css";
 
-export default function footer(){
+export default function Footer(){
     return(
         <footer className={Styles.footer}>
-            <nav>
+            <ul>
                 <li>
                     <Image
-                    width={100}
-                    heigth={100}
-                    src={"https://api.escuelajs.co/api/v1/products"}/>
-                    </li>
-            </nav>
+                    width={30}
+                    heigth={30}
+                    src={"https://i.pinimg.com/736x/7e/2d/f6/7e2df6d288a758c264186555d8809b57.jpg"}/>
+                    <spam>Instagram</spam>
+                </li>
+
+                <li>
+                    <Image
+                    width={30}
+                    heigth={30}
+                    src={"https://cdn-icons-png.flaticon.com/256/20/20673.png"}/>
+                     <spam>Facebook</spam>
+                </li>
+
+                <li>
+                    <Image
+                    width={30}
+                    heigth={30}
+                    src={"https://static.vecteezy.com/system/resources/thumbnails/031/737/197/small_2x/whatsapp-icon-whatsapp-social-media-logo-free-png.png"}/>
+                     <spam>Whatsapp</spam>
+                </li>
+
+                <li>
+                    <Image
+                    width={30}
+                    heigth={30}
+                    src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi6f3fz3-qVnSYcTy-ZnYTvmYGsVQKjlMNfQ&s"}/>
+                     <spam>TikTok</spam>
+                </li>
+                <Image
+                width={60}
+                heigth={60}
+                src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdLN2gs2TAHUiw0m1MjR4rvDt6dlJIklxNXA&s"}/>
+            </ul>
+
+            <p>Desde 22/02/2007</p>
             </footer>
     );
 };
--- src/app/componentes/Header.js
+++ src/app/componentes/Header.js
@@ -2,14 +2,8 @@ import styles from "./header.module.css"
 export default function Header(){
 
     return (
-  <nav className={styles.navbar}>
-    <div className={styles.navbar_brand}>Shop Main</div>
-    <ul className={styles.navbar_menu}>
-     <li className={styles.navbar_item}><a href="#home">Início</a></li>
-     <li className={styles.navbar_item}><a href="#">Início</a></li>
-     <li className={styles.navbar_item}><a href="#">Início</a></li>
-     <li className={styles.navbar_item}><a href="#">Início</a></li>
-     </ul>
-     </nav>
+  <header className={styles.header}>
+    <h1>Colo de Deus</h1>
+    </header>
     );
 };
